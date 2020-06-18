9847e4ddf771abf8afd9825da68f7ffb55bc9773
michael emmons <memmons18@gmail.com>
2020-06-18 13:53:39 -0400

    changed costco api call

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -13,15 +13,17 @@ module.exports = async function (app, cheerio) {
       let doa = await newRequest(
         "https://www.dealsofamerica.com/laptop-deals.php"
       );
-      let BB = await newRequest(
-        "https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=laptopscreensizerange_facet%3DScreen%20Size~16%22%20or%20More&sc=Global&st=laptop&type=page&usc=All%20Categories"
+      // let BB = await newRequest(
+      //   "https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=laptopscreensizerange_facet%3DScreen%20Size~16%22%20or%20More&sc=Global&st=laptop&type=page&usc=All%20Categories"
+      // );
+      let costco = await newRequest(
+        "https://www.costco.com/laptops.html?operating-system=windows-os&refine=ads_f117501_ntk_cs%253A%2522Windows%2BOS%2522|"
       );
-      // let costco = await newRequest("https://www.costco.com/laptops.html?operating-system=windows-os&refine=ads_f117501_ntk_cs%253A%2522Windows%2BOS%2522|")
       // let staple = await newRequest("https://www.staples.com/17-18-9--Laptops-Deals/cat_CL167289/00792-41gw7")
 
       await scraperSite.dealsOfA(doa);
-      await scraperSite.bestBuySearch(BB);
-      // await scraperSite.costcoSearch(costco);
+      // await scraperSite.bestBuySearch(BB);
+      await scraperSite.costcoSearch(costco);
       // await scraperSite.staplesSearch(staple);
 
       res.json(scraperSite.company);
